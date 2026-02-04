e8e983b9bd13d6abbbb70408b7d68f8d200d0a2a
Samah Naji <samah.naji.1@ulaval.ca>
2026-02-03 22:56:26 -0500

    fix docker compose

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,13 @@
+-- Create database if not exists
 IF DB_ID('proposed_2025_11') IS NULL
 BEGIN
-  CREATE DATABASE proposed_2025_11;
+    CREATE DATABASE proposed_2025_11;
 END
+GO
+
+USE proposed_2025_11;
+GO
+
+-- Import the real schema + data
+:r /sql_generation_scripts/2025-11-27_proposed_.sql
 GO
